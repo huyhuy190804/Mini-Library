0571487274f8f6cb864dd1f0e69497eaa2783535
Huynh Van Huy <huynhhuy190804@gmail.com>
2026-04-03 19:13:00 +0700

    update server+

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,35 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js'; // Nhớ đuôi .js
-import borrowingRoutes from './routes/borrowingRoute.js'; // Nhớ đuôi .js
-import bookRoutes from './routes/bookRoute.js'; // Nhớ đuôi .js
-import authorRoutes from './routes/authorRoute.js'; // Nhớ đuôi .js
-import statsRoutes from './routes/statsRoute.js'; // Nhớ đuôi .js
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js"; // Nhớ đuôi .js
+import borrowingRoutes from "./routes/borrowingRoute.js"; // Nhớ đuôi .js
+import bookRoutes from "./routes/bookRoute.js"; // Nhớ đuôi .js
+import authorRoutes from "./routes/authorRoute.js"; // Nhớ đuôi .js
+import statsRoutes from "./routes/statsRoute.js"; // Nhớ đuôi .js
 
 // Load biến môi trường
 dotenv.config();
 
 const app = express();
 const allowedOrigins = [
-  "https://mini-library-kn492sf4z-huyhuy190804s-projects.vercel.app",
+  "https://mini-library-louishuy.vercel.app",
   "https://mini-library-git-main-huyhuy190804s-projects.vercel.app",
-  "http://localhost:3000"
+  "http://localhost:3000",
 ];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (
-        !origin || allowedOrigins.includes(origin)
-      ) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Origin not allowed"));
       }
     },
     credentials: true,
-  })
+  }),
 );
 
 // Middleware quan trọng: Giúp Express hiểu được dữ liệu JSON
@@ -41,10 +38,10 @@ connectDB();
 
 // Route gốc
 // Mọi request bắt đầu bằng /api/borrowings sẽ đi vào borrowingRoutes
-app.use('/api/borrowings', borrowingRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/authors', authorRoutes);
-app.use('/api/stats', statsRoutes);
+app.use("/api/borrowings", borrowingRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/authors", authorRoutes);
+app.use("/api/stats", statsRoutes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
